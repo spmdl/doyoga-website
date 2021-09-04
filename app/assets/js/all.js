36ad17c8e5a39f5ca0555c5ec798b8d43ddc5bd0
Spm-Eng <dlspm0512@gmail.com>
2021-09-04 23:39:39 +0800

    feat: index page swiper navigation

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,21 +1,21 @@
 const swiper = new Swiper('.courseSwiper', {
+  slidesPerView: 2,
+  spaceBetween: 15,
+  grid: {
+    fill: 'row',
+    rows: 2,
+  },
   breakpoints: {
+    768: {
+      spaceBetween: 30,
+    },
     992: {
       slidesPerView: 1.38,
       spaceBetween: 30,
       grid: {
         rows: 1,
       },
-    },
-    768: {
-      spaceBetween: 30,
     }
-  },
-  slidesPerView: 2,
-  spaceBetween: 15,
-  grid: {
-    fill: 'row',
-    rows: 2,
   },
   // 為了解決 PC 版需要 autoplay
   autoplay: {
@@ -25,7 +25,35 @@ const swiper = new Swiper('.courseSwiper', {
 });
 
 const feedbackSwiper = new Swiper(".feedbackSwiper", {
-
+  slidesPerView:1,
+  grid :{
+    fill:'row',
+    rows:3,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.12,
+      spaceBetween:30,
+      grid:{
+        rows:2,
+      }
+    },
+    992:{
+      slidesPerView: 3,
+      spaceBetween:30,
+      grid:{
+        rows:2,
+      }
+    }
+  },
+  navigation: {
+    prevEl: ".swiper-btn-prev",
+    nextEl: ".swiper-btn-next"
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  }
 });
 
 
