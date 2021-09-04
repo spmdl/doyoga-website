803f40b33f0d8778433da568df59a785ada14c85
Spm-Eng <dlspm0512@gmail.com>
2021-09-04 14:12:42 +0800

    feat: index page course swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,29 @@
+var swiper = new Swiper('.courseSwiper', {
+  breakpoints: {
+    992: {
+      slidesPerView: 1.5,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+      },
+    },
+    768: {
+      spaceBetween: 30,
+    }
+  },
+  slidesPerView: 2,
+  spaceBetween: 15,
+  grid: {
+    fill: 'row',
+    rows: 2,
+  },
+  // 為了解決 PC 版需要 autoplay
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false
+  // }
+});
+
 $(function() {
   console.log('Hello Bootstrap5');
 });
