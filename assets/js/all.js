ab42825f0f5073d9d4cf56ab38387976fd869388
Spm-Eng <dlspm0512@gmail.com>
2021-09-04 23:39:54 +0800

    Update 2021-09-04T15:39:51.815Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,23 +1,23 @@
 "use strict";
 
 var swiper = new Swiper('.courseSwiper', {
+  slidesPerView: 2,
+  spaceBetween: 15,
+  grid: {
+    fill: 'row',
+    rows: 2
+  },
   breakpoints: {
+    768: {
+      spaceBetween: 30
+    },
     992: {
       slidesPerView: 1.38,
       spaceBetween: 30,
       grid: {
         rows: 1
       }
-    },
-    768: {
-      spaceBetween: 30
     }
-  },
-  slidesPerView: 2,
-  spaceBetween: 15,
-  grid: {
-    fill: 'row',
-    rows: 2
   },
   // 為了解決 PC 版需要 autoplay
   autoplay: {
@@ -25,7 +25,37 @@ var swiper = new Swiper('.courseSwiper', {
     disableOnInteraction: false
   }
 });
-var feedbackSwiper = new Swiper(".feedbackSwiper", {});
+var feedbackSwiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: 1,
+  grid: {
+    fill: 'row',
+    rows: 3
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.12,
+      spaceBetween: 30,
+      grid: {
+        rows: 2
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      grid: {
+        rows: 2
+      }
+    }
+  },
+  navigation: {
+    prevEl: ".swiper-btn-prev",
+    nextEl: ".swiper-btn-next"
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  }
+});
 $(function () {
   console.log('Hello Bootstrap5');
 });
