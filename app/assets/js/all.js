d1b13ae3cd7830e67710687bcb6442e124db245f
Spm-Eng <dlspm0512@gmail.com>
2021-09-12 11:49:35 +0800

    feat: datepicker plugin

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -144,6 +144,17 @@ const teachersSwiper = new Swiper(".teachersSwiper", {
   },
 });
 
+const dateDom = document.querySelector('.js-datepicker-date');
+if (dateDom) {
+  const datepicker = new Datepicker(dateDom, {
+    autohide: true,
+    daysOfWeekDisabled: [0],
+    nextArrow: '>',
+    prevArrow: '<',
+    buttonClass: 'btn btn-outline-primary border-0 fw-normal',
+  });
+}
+
 $(function() {
   console.log('Hello Bootstrap5');
 });
